df3ac3ce2a04aba7f868db3a3a58a736a6948858
Patryk Smakosz <pas@baernholdt.com>
2023-12-16 04:54:17 +0100

    netlify fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    modules: ["@nuxtjs/tailwindcss", "@hypernym/nuxt-gsap","@pinia/nuxt"],
     plugins: [
         '~/plugins/route-cleanup.js'
     ],
-    modules: ["@nuxtjs/tailwindcss", "@hypernym/nuxt-gsap","@pinia/nuxt"],
-});
+    ssr: true,
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+            cssnano:
+                process.env.NODE_ENV === 'production'
+                    ? { preset: ['default', { discardComments: { removeAll: true } }] }
+                    : false, // disable cssnano when not in production
+        },
+    },
+})
