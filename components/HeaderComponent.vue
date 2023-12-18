311fd514cad29b0b75cb73ecdae4f70a292341b3
Patryk Smakosz <pas@baernholdt.com>
2023-12-18 15:41:13 +0100

    fix build netlify

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -63,25 +63,27 @@
   </header>
 </template>
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue';
+import {onMounted, onUnmounted, ref} from 'vue';
 import gsap from 'gsap';
-import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
+import {MorphSVGPlugin} from 'gsap/MorphSVGPlugin';
 import useTextSlider from '~/composables/useTextSlider';
 import useThemeSwitch from '~/composables/useThemeSwitch';
 import useHeadroom from '~/composables/useHeadroom';
 import ThemeToggleSVG from "~/components/ThemeToggleSVG.vue";
 
 // Initialize composable functions
-const { play: playSlider1,progress: progressSlider1 } = useTextSlider('.js--text-slider-01 li');
-const { play: playSlider2,progress: progressSlider2 } = useTextSlider('.js--text-slider-02 li');
-const { play: playSlider3,progress: progressSlider3 } = useTextSlider('.js--text-slider-03 li');
+const {play: playSlider1, progress: progressSlider1} = useTextSlider('.js--text-slider-01 li');
+const {play: playSlider2, progress: progressSlider2} = useTextSlider('.js--text-slider-02 li');
+const {play: playSlider3, progress: progressSlider3} = useTextSlider('.js--text-slider-03 li');
 
-const { initHeadroom, destroyHeadroom } = useHeadroom('.nav');
+const {initHeadroom, destroyHeadroom} = useHeadroom('.nav');
 
-gsap.registerPlugin(MorphSVGPlugin);
+if (process.client) {
+  gsap.registerPlugin(MorphSVGPlugin);
 
+}
 onMounted(() => {
-  const { initThemeSwitch } = useThemeSwitch();
+  const {initThemeSwitch} = useThemeSwitch();
 
   MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
   playSlider1();
